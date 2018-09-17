a164c9989732b897bb2005b4a04d85cdbd142a1b
Stjepan Lovasic <stjepan.lovasic@gmail.com>
2018-09-17 21:43:17 +0200

    Updated gitignore, added basic models

--- rental/apartments/models.py
+++ rental/apartments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Apartment(models.Model):
+
+    def __str__(self):
+        return
+
+    def __unicode__(self):
+        return
+
+
+class Reservation(models.Model):
+
+    def __str__(self):
+        return
+
+    def __unicode__(self):
+        return
